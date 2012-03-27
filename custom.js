486868e25e7ac950417dab72e11a24cdab678d7d
Misha Krit <millianorb@gmail.com>
2012-03-27 23:17:27 +0300

    Add comments to script file

--- custom.js
+++ custom.js
@@ -21,71 +21,84 @@
  
 			var elements = this;
 			
+			
+			/*Дефолтные значения параметров*/
             var defaults = {
 				customStyleClass:"checkbox",
 				customHeight:"16"
             }
-                 
+            
+			/*Заменяем дефолтные опции на переданные если таковые есть*/
             var options =  $.extend(defaults, options);
 		
+			/*Вид при нажатии на активный и неактивный элементы*/
 			var pushed = function(){
 	
 				var element = $(this).children('input');
 				if(element.is(':checked')){
-					$(this).css('backgroundPosition', "0 -" + options.customHeight*3 + "px");
+					/*смещения в спрайте*/
+					$(this).css('backgroundPosition', "0px -" + options.customHeight*3 + "px"); 
 				}else{
-					$(this).css('backgroundPosition', "0 -" + options.customHeight + "px");
+					$(this).css('backgroundPosition', "0px -" + options.customHeight + "px");
 				};
 			
 			};
 			
+			/*Отмечаем нажатый елемент все остальные сбрасываем, если они в групе(radio)*/
 			var check = function(){
 				var element = $(this).children('input');
-				if(element.is(':checked') && element.attr('type') === 'checkbox'){
-					$(this).css('backgroundPosition', '0 0');
-					$(this).children('input').attr('checked', false).change();
+				if(element.is(':checked') && element.attr('type') === 'checkbox'){/*Отмеченный чекбокс*/
+					$(this).css('backgroundPosition', '0px 0px');
+					$(this).children('input').attr('checked', false).change();/*Меняем атрибут на неотмеченный и вызываем событие смены состояния элемента*/
 				}else{
-					if(element.attr('type') === 'checkbox'){
-						$(this).css('backgroundPosition', "0 -" + options.customHeight*2 + "px");
+					if(element.attr('type') === 'checkbox'){/*Неотмеченный чекбокс*/
+						$(this).css('backgroundPosition', "0px -" + options.customHeight*2 + "px");
 					}else{
-						$(this).css('backgroundPosition', "0 -" + options.customHeight*2 + "px");
-						$('input[name='+ element.attr('name')+']').not(element).parent().css('backgroundPosition', '0 0');
+						/*Радиобатоны*/
+						$(this).css('backgroundPosition', "0px -" + options.customHeight*2 + "px");
+						$('input[name='+ element.attr('name')+']').not(element).parent().css('backgroundPosition', '0px 0px');
 					};
-					$(this).children('input').attr('checked','checked').change();
+					
+					$(this).children('input').attr('checked','checked').change(); 
 				};
 			
 			};
 			
-			var clear = function(){
-				elements.each(function(){
-					if($(this).is(':checked')){
-						$(this).parent().css('backgroundPosition', "0 -" + options.customHeight*2 + "px");
-					}else{
-						$(this).parent().css('backgroundPosition', "0 0");
-					};
-					
-				});
 			
+			/*Обновление картинки при клике по лейблу и загрузке документа*/
+			var update = function(){
+				elements.each(function(){ /*Проходим по всем елементам и проверяем их состояние*/
+					if($(this).is(':checked')){
+						$(this).parent().css('backgroundPosition', "0px -" + options.customHeight*2 + "px");
+					}else{
+						$(this).parent().css('backgroundPosition', "0px 0px");
+					};
+				});
 			};
 			
  
             return this.each(function() {
-				$(this).wrap('<span/>');
-				var span = $(this).parent().addClass(options.customStyleClass);
+				$(this).wrap('<span/>');/*Оборачиваем в <span></span>*/
+				var span = $(this).parent().addClass(options.customStyleClass);/*Приписываем класс оформления переданный в параметрах*/
+				/*Задаем картинку еси элемент отмечен*/
 				if($(this).is(':checked') === true) {
-					span.css('backgroundPosition', "0 -" + (options.customHeight*2) + "px");
+					span.css('backgroundPosition', "0px -" + (options.customHeight*2) + "px");
 				};
 				
-				$(this).change(clear);
+				/*Бинд на изменение состояния элемента*/
+				$(this).change(update);
 				
 				if(!$(this).prop('disabled')){
+					/*Бинд функция на span*/
 					span.mousedown(pushed);
 					span.mouseup(check);
 				}else{
+					/*Добавление класса если элемент неактивен*/
 					span.addClass('disabled');
 				};
-
-				$(document).mouseup(clear);
+				
+				/*Обновляем состояния (клика по лейблу)*/
+				$(document).mouseup(update);
              
             });
         }
